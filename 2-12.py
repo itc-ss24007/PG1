fe262decd3c3312591e1007228d1c7026ef171c9
itc-ss24007 <s24007@std.it-college.ac.jp>
2024-07-09 06:34:16 +0000

    chapter2 finished

---
+++ 2-12.py
@@ -0,0 +1,9 @@
+import random
+print(random.randint(0,10))
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(random.choice(alphabet))
+print(random.sample(alphabet,5))
+
+import my_module
+print(my_module.func(5))
