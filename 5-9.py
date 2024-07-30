0d629e349598187562e174dc49e87a468dda92ce
itc-ss24007 <s24007@std.it-college.ac.jp>
2024-07-30 04:57:42 +0000

    辞書

---
+++ 5-9.py
@@ -0,0 +1,11 @@
+mountain_in_japan = {'fuji': 3776, 'kitadake':3193,'okuhodakadake':3190}
+mountain_in_japan_sorted = sorted(mountain_in_japan.items(),key=lambda x : x[0])
+for key,val in mountain_in_japan_sorted:
+    print(key,val)
+
+mountain_in_japan_sorted = sorted(mountain_in_japan.items(),key=lambda x : x[1])
+for key,val in mountain_in_japan_sorted:
+    print(key,val)
+
+for key,val in reversed(mountain_in_japan_sorted):
+    print(key,val)
